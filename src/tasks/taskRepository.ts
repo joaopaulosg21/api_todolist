d54eda120f3c17363ebbee372fd04d845b8da918
JoãoPaulo <joaopaulosg17@gmail.com>
2022-06-25 19:40:15 -0300

    teste de deploy

--- src/tasks/taskRepository.ts
+++ src/tasks/taskRepository.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, tasks } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { Itask } from "./types/Itask";
 
 export class TaskRepository {
@@ -7,7 +7,7 @@ export class TaskRepository {
         this.model = new PrismaClient();
     }
 
-    public async newTask(task:Itask):Promise<tasks>{
+    public async newTask(task:Itask):Promise<any>{
         return this.model.tasks.create({
             data:{
                 title:task.title,
@@ -16,33 +16,33 @@ export class TaskRepository {
         });
     }
 
-    public async getTaskByTitle(taskTitle:string):Promise<tasks | null>{
+    public async getTaskByTitle(taskTitle:string):Promise<any | null>{
         return this.model.tasks.findFirst({where:{title:taskTitle}});
     }
 
-    public async getAllTasks():Promise<tasks[]> {
+    public async getAllTasks():Promise<any[]> {
         return this.model.tasks.findMany();
     }
 
-    public async getAllTasksSortByCreatedDate(ordem:any):Promise<tasks[]> {
+    public async getAllTasksSortByCreatedDate(ordem:any):Promise<any[]> {
         return this.model.tasks.findMany({orderBy:[{createdAt:ordem}]});
     }
 
-    public async updateTaskStatus(taskId:number,status:string):Promise<tasks | null>{
+    public async updateTaskStatus(taskId:number,status:string):Promise<any | null>{
         return this.model.tasks.update({
             where:{id:taskId},
             data:{status:status}
         });
     }
     
-    public async finishTask(taskId:number,date:Date):Promise<tasks | null>{
+    public async finishTask(taskId:number,date:Date):Promise<any | null>{
         return this.model.tasks.update({
             where:{id:taskId},
             data:{endedIn:date,status:"concluido"}
         });
     }
 
-    public async getTaskById(taskId:number):Promise<tasks | null>{
+    public async getTaskById(taskId:number):Promise<any | null>{
         return this.model.tasks.findFirst({where:{id:taskId}});
     }
 }
